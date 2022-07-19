f5ebd95a0802afc8d949098a5a93bce2ec065ea4
Woraes Will Home <moraeswilli@hotmail.com>
2022-07-18 22:13:12 -0300

    22:12 18/07/22

--- Moments/src/app/app.module.ts
+++ Moments/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { MomentFormComponent } from './components/moment-form/moment-form.component';
-import { NewMomentComponent } from './components/new-moment/new-moment.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
 @NgModule({
   declarations: [
